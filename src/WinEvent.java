a33e58d6e025bb8e6393f40edd0edd31e0c67eae
Mustafa Haider <haidermustafa9000@gmail.com>
2024-04-16 23:53:19 -0400

    more changes

--- src/WinEvent.java
+++ src/WinEvent.java
@@ -1,25 +1,19 @@
 import java.io.*;
 
 public class WinEvent extends Event {
-    private static final String VICTORY_SAVE_FILE = "victory";  
-    private static final String SAVE_FILE_EXTENSION = ".sav"; 
 
-    private String winMessage = "You have won! Congratulations! Your game has been saved to ";
 
+    private String winMessage = "You won, game saved to ";
+   
     public WinEvent() {
     }
 
     @Override
     void trigger() {
         GameState instance = GameState.instance();
-        String saveFilePath = instance.getFullSaveName(VICTORY_SAVE_FILE + SAVE_FILE_EXTENSION);
-
         try {
-            instance.store(saveFilePath);
-            System.out.println(winMessage + saveFilePath);
-        } catch (IOException e) {
-            System.out.println("Failed to save the game: " + e.getMessage());
-        }
+            instance.store(instance.DEFAULT_SAVE_FILE);
+        } catch (IOException e) {}
+        System.out.println(winMessage + instance.DEFAULT_SAVE_FILE);
     }
 }
-
